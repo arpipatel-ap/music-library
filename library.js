f8860c488f2c4c5a0899ce088e4bca0e293c1721
arpipatel-ap <arpi96@gmail.com>
2024-05-04 11:11:38 -0400

    Add new function to manage playlists

--- library.js
+++ library.js
@@ -31,9 +31,12 @@ const library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 const printPlaylists = function() {
-
-}
-
+       for (const playlistId in library.playlists) {
+       const playlist = library.playlists[playlistId];
+        console.log(`${playlistId}: ${playlist.name} - ${playlist.tracks.length} tracks`);
+       }
+ };
+ printPlaylists();
 
 // prints a list of all tracks, using the following format:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
