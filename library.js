97e12008ce994201b50ee4537caab6398aded621
Arpi Patel <143440073+arpipatel-ap@users.noreply.github.com>
2024-05-07 23:09:00 -0400

    library.js

--- library.js
+++ library.js
@@ -74,65 +74,17 @@ const library = {
 
 };
 
-/////////////////////////////
-// FUNCTIONS TO IMPLEMENT:
-/////////////////////////////
-
-// prints a list of all playlists, in the form:
-// p01: Coding Music - 2 tracks
-// p02: Other Playlist - 1 tracks
-
 library.printPlaylists();
-
-
-// prints a list of all tracks, using the following format:
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-// t03: Four Thirty-Three by John Cage (Woodstock 1952)
-
 library.printPlaylists();
-
-
-
-// prints a list of tracks for a given playlist, using the following format:
-// p01: Coding Music - 2 tracks
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-
 library.printPlaylist("p01");        
-
-
 library.printTrack("t01");
 library.printTrack("t02");
-
-
-
-// adds an existing track to an existing playlist
-
 library.addTrackToPlaylist("t03", "p01");
-
-// generates a unique id
-// (already implemented: use this for addTrack and addPlaylist)
 
  const generateUid  = function() {
        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 };
 
-
-// adds a track to the library
-
 library.addTrack("New Song", "New Artist", "New Album");
-
-
-// adds a playlist to the library
-
 library.addPlaylist("My Playlist");
 
-// STRETCH:
-// given a query string string, prints a list of tracks
-// where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri") 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
-const printSearchResults = function(query) {
-
-}
